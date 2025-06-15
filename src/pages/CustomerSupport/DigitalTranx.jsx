5044cc3d5d0a98719fbed3f4bc4f6c40781e0e7f
vipin9575 <vipin9575@gmail.com>
2025-06-15 09:37:34 +0530

    remainingIMagesADDED

--- src/pages/CustomerSupport/DigitalTranx.jsx
+++ src/pages/CustomerSupport/DigitalTranx.jsx
@@ -15,12 +15,21 @@ import img3 from "../../assets/images/Picture7.png";
 import img4 from "../../assets/images/Picture8.png";
 import img5 from "../../assets/images/Picture9.png";
 import img6 from "../../assets/images/Picture10.png";
+import img7 from "../../assets/images/Picture11.png";
+import img8 from "../../assets/images/Picture12.png";
+import img9 from "../../assets/images/Picture13.png";
+import img10 from "../../assets/images/Picture14.png";
+import img11 from "../../assets/images/Picture15.png";
+import img12 from "../../assets/images/Picture16.png";
+import img13 from "../../assets/images/Picture17.png";
+import img14 from "../../assets/images/Picture18.png";
+import img15 from "../../assets/images/Picture19.png";
+import img16 from "../../assets/images/Picture20.png";
 import TitleImageCard from "../../data/TitleImageCard";
 
 const imageList1 = [
   { url: img1, alt: "PFD" },
 ];
-
 const imageList2 = [
   { url: img2, alt: "P&ID" },
 ];
@@ -35,6 +44,28 @@ const imageList5 = [
 ];
 const imageList6 = [
   { url: img6, alt: "Detailed Control Schematics" },
+];
+const imageList7 = [
+  { url: img7, alt: "Cabinet GA" },
+];
+const imageList8 = [
+  { url: img8, alt: "Hydraulic Schematics" },
+];
+const imageList9 = [
+  { url: img9, alt: "By Laser Scanning" },
+  { url: img10, alt: "By Laser Scanning" },
+  { url: img11, alt: "By Laser Scanning" },
+];
+const imageList10 = [
+  { url: img12, alt: "By Photogrammetry" },
+  { url: img13, alt: "By Photogrammetry" },
+];
+const imageList11 = [
+  { url: img14, alt: "Smart 3D model conversion" },
+  { url: img15, alt: "Smart 3D model conversion" },
+];
+const imageList12 = [
+  { url: img16, alt: "OTS" },
 ];
 
 const DigitalTranx = () => {
@@ -83,7 +114,7 @@ const DigitalTranx = () => {
 
       <Container maxWidth="lg" sx={{ py: 6 }}>
         <Box textAlign="center" mb={6}>
-          <Typography variant="h6" component="h1" gutterBottom>
+          <Typography variant="h5" component="h1" gutterBottom>
             Intelligent and Interactive Drawings (2D) for:
           </Typography>
 
@@ -93,6 +124,12 @@ const DigitalTranx = () => {
           <TitleImageCard title="Cause and Effect Drawings" images={imageList4} />
           <TitleImageCard title="Power Distribution Diagram (SLD)" images={imageList5} />
           <TitleImageCard title="Detailed Control Schematics" images={imageList6} />
+          <TitleImageCard title="Cabinet GA" images={imageList7} />
+          <TitleImageCard title="Hydraulic Schematics" images={imageList8} />
+          <TitleImageCard title="By Laser Scanning" images={imageList9} />
+          <TitleImageCard title="By Photogrammetry" images={imageList10} />
+          <TitleImageCard title="Smart 3D model conversion" images={imageList11} />
+          <TitleImageCard title="3D Walkthrough and Operator Training Simulator (OTS)" images={imageList12} />
         </Box>
       </Container>
 
