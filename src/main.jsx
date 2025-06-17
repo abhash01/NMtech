7fa03624c2ce096c46c6256213987a8a7166614f
vipin9575 <vipin9575@gmail.com>
2025-06-17 20:39:51 +0530

    calibre design file removed

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { theme } from "./theme/index.js";
 import { ThemeProvider } from "@emotion/react";
-import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
-// import "./styles/variable.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CalibreDesign from "./pages/CalibreDesign.jsx";
 import PageNotFound from "./components/PageNotFound/PageNotFound.jsx";
 import EngineeringSoftwareExpertise from "./pages/EngineeringExpertise/EngineeringSoftwareExpertise.jsx";
 import { ServiceInclude } from "./pages/ServiceInclude/ServiceInclude.jsx";
@@ -36,7 +33,6 @@ createRoot(document.getElementById("root")).render(
       <Router>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/calibre-design" element={<CalibreDesign />} />
           <Route path="/engineering-software-expertise" element={<EngineeringSoftwareExpertise />} />
           <Route path="/service-include" element={<ServiceInclude />} />
           <Route path="/kochi-refinery" element={<KochiRefineryProject />} />
